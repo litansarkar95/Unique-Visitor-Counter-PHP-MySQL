aed5c35c26cee43a78c4fdca42f6d094d8d96171
litansarkar95 <litansarkar95@gmail.com>
2021-02-15 09:41:02 +0600

    php mysql

---
+++ db/themindspeaks.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 15, 2021 at 04:37 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `themindspeaks`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unique_visitors`
+--
+
+CREATE TABLE `unique_visitors` (
+  `date` date NOT NULL,
+  `ip` text DEFAULT NULL,
+  `views` int(11) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `unique_visitors`
+--
+
+INSERT INTO `unique_visitors` (`date`, `ip`, `views`) VALUES
+('2021-02-04', '::1', 1),
+('2021-02-12', '3::1', 2),
+('2021-02-13', '1::1', 2),
+('2021-02-14', '::1', 1),
+('2021-02-15', '::1', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `unique_visitors`
+--
+ALTER TABLE `unique_visitors`
+  ADD PRIMARY KEY (`date`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$conn=mysqli_connect("localhost","root","","themindspeaks");
+ $date=date("Y-m-d");
+$userIP=$_SERVER['REMOTE_ADDR'];
+$query="SELECT * FROM unique_visitors where date='$date' ";
+$result=mysqli_query($conn,$query);
+
+  
+if($result->num_rows==0){
+   
+ $insertQuery="insert into unique_visitors(date,ip) values('$date','$userIP')";
+ mysqli_query($conn,$insertQuery);
+ 
+}else{
+   $row=$result->fetch_assoc(); 
+   if(!preg_match('/'.$userIP.'/i',$row['ip'])){
+       $newIP="$row[ip]$userIP";
+       $updateQuery="UPDATE unique_visitors set ip='$newIP' , views=views+1 where date='$date' ";
+       mysqli_query($conn,$updateQuery);
+   } 
+}
+
+
+
+?>
